e551353a61f2c18660200458365ee943ce340675
Reece4256 <rrogers32@hawkmail.hccfl.edu>
2024-11-22 13:18:15 -0500

    0.2

--- Final Project/Final Project 0.1.py
+++ Final Project/Final Project 0.1.py
@@ -6,13 +6,26 @@ def print_allowed_vehicles():
     print("Allowed Vehicles:")
     for vehicle in AllowedVehiclesList:
         print(vehicle)
+
+def search_vehicle():
+    search_query = input("Please Enter the full Vehicle name: ")
+    if search_query in AllowedVehiclesList:
+        print(f"{search_query} is an authorized vehicle.")
+    else:
+        print(f"{search_query} is not an authorized vehicle. If you received this in error, please check the spelling and try again.")
         
+
+
 # Function to display the menu
 def display_menu(): 
-        print("\nAutoCountry Vehicle Finder v0.1") 
-        print("1. PRINT all Allowed Vehicles") 
-        print("2. Exit") 
-        return input("Please enter your choice (1 or 2): ")
+        print("\n********************************") 
+        print("AutoCountry Vehicle Finder v0.2") 
+        print("********************************") 
+        print("1. PRINT all Authorized Vehicles") 
+        print("2. SEARCH for Authorized Vehicle") 
+        print("3. Exit") 
+        print("********************************") 
+        return input("Please enter your choice (1, 2, or 3): ")
         
 
 def main():
@@ -22,7 +35,9 @@ def main():
             print_allowed_vehicles() 
         
         elif choice == '2': 
-            print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+            search_vehicle()
+        elif choice == '3': 
+            print("Thank you for using the AutoCountry Vehicle Finder. Goodbye!")
             break
         else: 
             print("Invalid choice. Please try again.")
